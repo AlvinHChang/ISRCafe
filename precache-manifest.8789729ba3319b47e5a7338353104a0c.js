e8343cf129c4d0201676374d9bbf160622543d9b
AlvinHChang <solstory@gmail.com>
2019-05-03 15:26:51 -0500

    Update gh page root

---
+++ precache-manifest.8789729ba3319b47e5a7338353104a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "17f39b19cff972b26163",
+    "url": "/static/css/main.ed4a1799.chunk.css"
+  },
+  {
+    "revision": "17f39b19cff972b26163",
+    "url": "/static/js/main.17f39b19.chunk.js"
+  },
+  {
+    "revision": "d0c10f04c564c81f4c3d",
+    "url": "/static/js/1.d0c10f04.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb5d1da33569f3cc94894e715e22a8d8",
+    "url": "/static/media/PetrolIcon.bb5d1da3.png"
+  },
+  {
+    "revision": "84f9f08b7381dd54ef93adb418ac3705",
+    "url": "/static/media/afrocalebCrop.84f9f08b.png"
+  },
+  {
+    "revision": "4c83a07ea59227f1da9bc0ba27281119",
+    "url": "/index.html"
+  }
+];
