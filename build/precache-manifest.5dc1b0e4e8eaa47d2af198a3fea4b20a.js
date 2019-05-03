0f8513996d9bb10ac97fc7839e159a3365b6170b
AlvinHChang <solstory@gmail.com>
2019-05-03 14:06:06 -0500

    gh-pages?

---
+++ build/precache-manifest.5dc1b0e4e8eaa47d2af198a3fea4b20a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "27083c002a685c07855c",
+    "url": "/ISRCafe/static/css/main.ed4a1799.chunk.css"
+  },
+  {
+    "revision": "27083c002a685c07855c",
+    "url": "/ISRCafe/static/js/main.27083c00.chunk.js"
+  },
+  {
+    "revision": "d0c10f04c564c81f4c3d",
+    "url": "/ISRCafe/static/js/1.d0c10f04.chunk.js"
+  },
+  {
+    "revision": "22772ce1882e7bb82858",
+    "url": "/ISRCafe/static/js/runtime~main.22772ce1.js"
+  },
+  {
+    "revision": "bb5d1da33569f3cc94894e715e22a8d8",
+    "url": "/ISRCafe/static/media/PetrolIcon.bb5d1da3.png"
+  },
+  {
+    "revision": "84f9f08b7381dd54ef93adb418ac3705",
+    "url": "/ISRCafe/static/media/afrocalebCrop.84f9f08b.png"
+  },
+  {
+    "revision": "0262a652110361572fc6462c7798c04f",
+    "url": "/ISRCafe/index.html"
+  }
+];
--- src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyBSRyjqGht9DZIMlhjip8jPJFPQJmzdmxc",
-  discoveryDocs:
-    ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-  spreadsheetId: "1LC-ZQXFZdf3rad0CVvmN0oL5bfmP2GZEhR-UN4MuN8o",
-  // the range to grab information
-  spreadsheetRange: "OrderStatus!A3:E",
-};
